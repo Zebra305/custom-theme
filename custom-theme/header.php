874e75b2e8b068ca0b90f35c374ed63edd158d51
Mohammed <mohamed11rb@gmail.com>
2023-11-04 00:32:37 +0100

    Navbar is blue

--- custom-theme/header.php
+++ custom-theme/header.php
@@ -7,8 +7,9 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+            <div class="container-xxl">
+                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">MIND MAGNET MEDIA</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -17,7 +18,7 @@
                     wp_nav_menu( array(
                         'theme_location' => 'header-menu',
                         'container'      => false,
-                        'menu_class'     => 'navbar-nav me-auto mb-2 mb-lg-0',
+                        'menu_class'     => 'navbar-nav ms-auto mb-2 mb-lg-0',
                         'fallback_cb'    => '__return_false',
                         'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
                         'depth'          => 2,

e24682859ec7c19c8df331479d98373965fdd75d
Mohammed <mohamed11rb@gmail.com>
2023-11-04 17:49:30 +0100

    added spacing between the menu items

--- custom-theme/header.php
+++ custom-theme/header.php
@@ -18,7 +18,7 @@
                     wp_nav_menu( array(
                         'theme_location' => 'header-menu',
                         'container'      => false,
-                        'menu_class'     => 'navbar-nav ms-auto mb-2 mb-lg-0',
+                        'menu_class'     => 'navbar-nav ms-auto mb-2 mb-lg-0 px-3',
                         'fallback_cb'    => '__return_false',
                         'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
                         'depth'          => 2,
